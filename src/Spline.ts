547467ef09c1629d6052ad34d8792b5c614bd8c8
kunchtler <leo.kulinski@gmail.com>
2024-06-03 17:17:01 +0200

    CubicHermite update + QuinticHermite added

--- src/Spline.ts
+++ src/Spline.ts
@@ -2,43 +2,39 @@ import { Matrix, Structure } from "./Matrix";
 import { createRBTree, RBTree } from "./RBTree";
 import { NUMBERS_STRUCTURE } from "./constants";
 
-//TODO : Assumes at least 1 element.
 //TODO : To geneeralize to splines, rename points by parameter ?
+//TODO : Rewrite for threejs ?
 
-class CubicHermiteSpline<T> {
-    points: T[];
-    dpoints: T[];
-    knots: number[];
-    tree: RBTree<number, number>;
-    structure: Structure<T, number>;
+abstract class Spline<T> {
+    abstract knots: number[];
+    abstract tree: RBTree<number, number>;
+    abstract structure: Structure<T, number>;
 
-    constructor(structure: Structure<T, number>, points: T[], dpoints: T[], knots?: number[]) {
-        if (points.length !== dpoints.length) {
-            throw new Error("The number of points and derivatives should be the same.");
-        }
-        this.points = points;
-        this.dpoints = dpoints;
+    // constructor(structure: Structure<T, number>) {
+    //     // if (parameters.length === 0) {
+    //     //     throw new Error("A spline should have at least one set of parameters.");
+    //     // }
 
-        if (typeof knots === "undefined") {
-            this.knots = new Array<number>(this.points.length);
-            for (let i = 0; i < this.knots.length; i++) {
-                this.knots[i] = i;
-            }
-        } else {
-            if (points.length !== knots.length) {
-                throw new Error("Points and knots must have the same size");
-            }
-            this.knots = knots;
-        }
+    //     // this.parameters = parameters;
+    //     this.structure = structure;
 
-        let tree: RBTree<number, number> = createRBTree();
-        for (let i = 0; i < this.points.length; i++) {
-            tree = tree.insert(this.knots[i], i);
-        }
-        this.tree = tree;
+    //     // if (knots === undefined) {
+    //     //     this.knots = new Array<number>(this.parameters.length);
+    //     //     for (let i = 0; i < this.knots.length; i++) {
+    //     //         this.knots[i] = i;
+    //     //     }
+    //     // } else if (this.parameters.length !== knots.length) {
+    //     //     throw new Error("Points and knots must have the same size");
+    //     // } else {
+    //     //     this.knots = knots;
+    //     // }
 
-        this.structure = structure;
-    }
+    //     // let tree: RBTree<number, number> = createRBTree();
+    //     // for (let i = 0; i < this.parameters.length; i++) {
+    //     //     tree = tree.insert(this.knots[i], i);
+    //     // }
+    //     // this.tree = tree;
+    // }
 
     prev_next_time_idx(time: number): {
         prev_time: number;
@@ -46,14 +42,12 @@ class CubicHermiteSpline<T> {
         prev_idx: number;
         next_idx: number;
     } {
-        //TODO : It is taken care of in other functions that the time is in range. Remove this check ? This function ?
         if (time < this.knots[0]) {
             time = this.knots[0];
         } else if (time > this.knots[this.knots.length - 1]) {
             time = this.knots[this.knots.length - 1];
         }
 
-        // Interpolate value.
         const prev_it = this.tree.le(time);
         const next_it = this.tree.ge(time);
         const prev_time = prev_it.key;
@@ -77,43 +71,28 @@ class CubicHermiteSpline<T> {
         };
     }
 
-    get_transformation_matrix(dt: number): Matrix<number, number> {
-        const transformation_data = [
-            [2, -2, dt, dt],
-            [-3, 3, -2 * dt, -dt],
-            [0, 0, dt, 0],
-            [1, 0, 0, 0]
-        ];
-        return new Matrix<number, number>(transformation_data, NUMBERS_STRUCTURE);
-    }
+    abstract get_transformation_matrix(dt?: number): Matrix<number, number>;
 
-    get_param_matrix(prev_idx: number, next_idx: number) {
-        const param_data = [
-            [this.points[prev_idx]],
-            [this.points[next_idx]],
-            [this.dpoints[prev_idx]],
-            [this.dpoints[next_idx]]
-        ];
-        return new Matrix<T, number>(param_data, this.structure);
-    }
+    abstract get_param_matrix(prev_idx: number, next_idx: number, dt?: number): Matrix<T, number>;
 
     interpolate(time: number): T {
-        if (time <= this.knots[0]) {
-            return this.points[0];
-        } else if (time >= this.knots[this.knots.length - 1]) {
-            return this.points[this.points.length - 1];
-        }
         const { prev_time, next_time, prev_idx, next_idx } = this.prev_next_time_idx(time);
 
         // Creating transformation matrix
         const dt = next_time - prev_time;
         const transformation_mat = this.get_transformation_matrix(dt);
+
         // Creating parameters vector
-        const param_mat = this.get_param_matrix(prev_idx, next_idx);
+        const param_mat = this.get_param_matrix(prev_idx, next_idx, dt);
+
         // Creating vector of times
-        const time_nor = (time - prev_time) / (next_time - prev_time);
-        const times_data = [[time_nor ** 3, time_nor ** 2, time_nor ** 1, time_nor ** 0]];
-        const times_mat = new Matrix<number, number>(times_data, NUMBERS_STRUCTURE);
+        const time_nor = next_idx === prev_idx ? 0 : (time - prev_time) / dt;
+        const degree = transformation_mat.nb_row;
+        const times_data = Array<number>(degree);
+        for (let i = 0; i < times_data.length; i++) {
+            times_data[i] = time_nor ** (degree - 1 - i);
+        }
+        const times_mat = new Matrix<number, number>([times_data], NUMBERS_STRUCTURE);
 
         // Computing the interpolation. Result has only 1 element
         const res = Matrix.multiply_matrix_of_scalars_by_matrix(
@@ -124,22 +103,24 @@ class CubicHermiteSpline<T> {
     }
 
     velocity(time: number): T {
-        if (time <= this.knots[0]) {
-            return this.dpoints[0];
-        } else if (time >= this.knots[this.knots.length - 1]) {
-            return this.dpoints[this.points.length - 1];
-        }
         const { prev_time, next_time, prev_idx, next_idx } = this.prev_next_time_idx(time);
 
         // Creating transformation matrix
         const dt = next_time - prev_time;
         const transformation_mat = this.get_transformation_matrix(dt);
+
         // Creating parameters vector
-        const param_mat = this.get_param_matrix(prev_idx, next_idx);
+        const param_mat = this.get_param_matrix(prev_idx, next_idx, dt);
+
         // Creating vector of times
-        const time_nor = (time - prev_time) / (next_time - prev_time);
-        const times_data = [[3 * time_nor ** 2, 2 * time_nor, 1, 0]];
-        const times_mat = new Matrix<number, number>(times_data, NUMBERS_STRUCTURE);
+        const time_nor = next_idx === prev_idx ? 0 : (time - prev_time) / dt;
+        const degree = transformation_mat.nb_row;
+        const times_data = Array<number>(degree);
+        for (let i = 0; i < times_data.length - 1; i++) {
+            times_data[i] = time_nor ** (degree - 2 - i);
+        }
+        times_data[times_data.length - 1] = 0;
+        const times_mat = new Matrix<number, number>([times_data], NUMBERS_STRUCTURE);
 
         // Computing the interpolation. Result has only 1 element
         const res = Matrix.multiply_matrix_of_scalars_by_matrix(
@@ -150,52 +131,134 @@ class CubicHermiteSpline<T> {
     }
 }
 
-// /**
-//  * Interpolates the non uniform cubic hermite spline given the following :
-//  * @param pos0 the initial value.
-//  * @param dpos0 the velocity at that first value.
-//  * @param t0 the time at that first value.
-//  * @param pos1 the final value.
-//  * @param dpos1 the velocity at that final value.
-//  * @param t1 the time at that final value.
-//  * @param t the time (between t0 and t1) to interpolate.
-//  * @returns the interpolated value.
-//  */
-// function cubic_hermite_spline<T extends math.Matrix | >(
-//     pos0: math.Matrix,
-//     dpos0: math.Matrix,
-//     t0: number,
-//     pos1: math.Matrix,
-//     dpos1: math.Matrix,
-//     t1: number,
-//     t: number
-// ): math.Matrix {
-//     const dt = t1 - t0;
-//     const mat = math.matrix([
-//         [2, -2, dt, dt],
-//         [-3, 3, -2 * dt, dt],
-//         [0, 0, 1, 0],
-//         [1, 0, 0, 0]
-//     ]);
-//     const params_mat = [pos0, pos1, dpos0, dpos1];
-//     const t_nor = (t - t0) / (t1 - t0);
-//     const times_mat = math.matrix([t_nor ** 3, t_nor ** 2, t_nor ** 1, t_nor ** 0]);
-//     return multiply_matrix_of_scalars_by_vector_of_matrices(
-//         math.multiply(times_mat, mat),
-//         params_mat
-//     )[0];
-// }
+class CubicHermiteSpline<T> extends Spline<T> {
+    points: T[];
+    dpoints: T[];
+    knots: number[];
+    tree: RBTree<number, number>;
+    structure: Structure<T, number>;
+    private static _transformation_matrix = new Matrix<number, number>(
+        [
+            [2, -2, 1, 1],
+            [-3, 3, -2, -1],
+            [0, 0, 1, 0],
+            [1, 0, 0, 0]
+        ],
+        NUMBERS_STRUCTURE
+    );
 
-// function three_vector3_to_matjs_matrix(vec: THREE.Vector3Like): math.Matrix {
-//     return math.matrix([vec.x, vec.y, vec.z]);
-// }
+    constructor(structure: Structure<T, number>, points: T[], dpoints: T[], knots?: number[]) {
+        super();
+        if (points.length !== dpoints.length) {
+            throw new Error("The number of points and derivatives should be the same.");
+        }
+        this.points = points;
+        this.dpoints = dpoints;
 
-// function matjs_matrix_to_three_vector3(mat: math.Matrix): THREE.Vector3 {
-//     return new THREE.Vector3(
-//         mat.get([0]) as number,
-//         mat.get([1]) as number,
-//         mat.get([2]) as number
-//     );
-// }
+        if (knots === undefined) {
+            this.knots = new Array<number>(this.points.length);
+            for (let i = 0; i < this.knots.length; i++) {
+                this.knots[i] = i;
+            }
+        } else if (points.length !== knots.length) {
+            throw new Error("Points and knots must have the same size");
+        } else {
+            this.knots = knots;
+        }
 
-export { CubicHermiteSpline };
+        let tree: RBTree<number, number> = createRBTree();
+        for (let i = 0; i < this.points.length; i++) {
+            tree = tree.insert(this.knots[i], i);
+        }
+        this.tree = tree;
+
+        this.structure = structure;
+    }
+
+    get_transformation_matrix(): Matrix<number, number> {
+        return CubicHermiteSpline._transformation_matrix;
+    }
+
+    get_param_matrix(prev_idx: number, next_idx: number, dt: number) {
+        const param_data = [
+            [this.points[prev_idx]],
+            [this.points[next_idx]],
+            [this.structure.multiply_by_scalar(dt, this.dpoints[prev_idx])],
+            [this.structure.multiply_by_scalar(dt, this.dpoints[next_idx])]
+        ];
+        return new Matrix<T, number>(param_data, this.structure);
+    }
+}
+
+class QuniticHermiteSpline<T> extends Spline<T> {
+    points: T[];
+    dpoints: T[];
+    apoints: T[];
+    knots: number[];
+    tree: RBTree<number, number>;
+    structure: Structure<T, number>;
+    private static _transformation_matrix = new Matrix<number, number>(
+        [
+            [-6, 6, -3, -3, -1 / 2, 1 / 2],
+            [15, -15, 8, 7, 3 / 2, -1],
+            [-10, 10, -6, -4, -3 / 2, 1 / 2],
+            [0, 0, 0, 0, 1 / 2, 0],
+            [0, 0, 1, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0]
+        ],
+        NUMBERS_STRUCTURE
+    );
+
+    constructor(
+        structure: Structure<T, number>,
+        points: T[],
+        dpoints: T[],
+        apoints: T[],
+        knots?: number[]
+    ) {
+        super();
+        if (points.length !== dpoints.length || points.length !== apoints.length) {
+            throw new Error("The number of points and derivatives should be the same.");
+        }
+        this.points = points;
+        this.dpoints = dpoints;
+        this.apoints = apoints;
+
+        if (knots === undefined) {
+            this.knots = new Array<number>(this.points.length);
+            for (let i = 0; i < this.knots.length; i++) {
+                this.knots[i] = i;
+            }
+        } else if (points.length !== knots.length) {
+            throw new Error("Points and knots must have the same size");
+        } else {
+            this.knots = knots;
+        }
+
+        let tree: RBTree<number, number> = createRBTree();
+        for (let i = 0; i < this.points.length; i++) {
+            tree = tree.insert(this.knots[i], i);
+        }
+        this.tree = tree;
+
+        this.structure = structure;
+    }
+
+    get_transformation_matrix(): Matrix<number, number> {
+        return QuniticHermiteSpline._transformation_matrix;
+    }
+
+    get_param_matrix(prev_idx: number, next_idx: number, dt: number) {
+        const param_data = [
+            [this.points[prev_idx]],
+            [this.points[next_idx]],
+            [this.structure.multiply_by_scalar(dt, this.dpoints[prev_idx])],
+            [this.structure.multiply_by_scalar(dt, this.dpoints[next_idx])],
+            [this.structure.multiply_by_scalar(dt ** 2, this.apoints[next_idx])],
+            [this.structure.multiply_by_scalar(dt ** 2, this.apoints[prev_idx])]
+        ];
+        return new Matrix<T, number>(param_data, this.structure);
+    }
+}
+
+export { CubicHermiteSpline, QuniticHermiteSpline };
